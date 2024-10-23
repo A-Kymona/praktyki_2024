426d5800ddc7c8b06d5c3f3a1b0b770a3d0fa392
A-Kymona <antek_kymona@wp.pl>
2024-10-23 11:42:11 +0200

    Dodanie dwóch zadań z C#

---
+++ Zadania_c#/zad_1/moje_zad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace zad_1
+
+{
+    class moje_zad
+    {
+        //Aplikacja konsoli C#, która pobiera od użytkownika jakiś tekst i zlicza ilość wystąpień znaku również podanego przez użytkownika
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj tekst:");
+            string inputText = Console.ReadLine();
+
+            Console.Write("\nPodaj znak do zliczenia:");
+            char characterToCount = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            int count = 0;
+            foreach (char c in inputText)
+            {
+                if (c == characterToCount)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"\nZnak '{characterToCount}' występuje {count} razy w podanym tekście.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zadania_c#/zad_2/Tomek.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Tomek
+{
+    private static void Main(string[] args)
+    {
+        //Aplikacja konsoli C# która po wczytaniu z klawiatury 3 liczb całkowitych wypisze na ekran ich sumę oraz informację czy ich reszta z dzielenia jest równa 0.
+        int liczba1, liczba2, liczba3, suma;
+        Console.Write("Podaj pierwszą liczbę:");
+        liczba1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nPodaj drugą liczbę:");
+        liczba2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nPodaj trzecią liczbę:");
+        liczba3 = Convert.ToInt32(Console.ReadLine());
+                
+        suma = liczba1 + liczba2 + liczba3;
+        if (suma % 3 == 0)
+        {
+            Console.WriteLine("\nSuma trzech podanych przez Ciebie liczb wynosi " + suma + ". I jest podzielna przez 3");
+        }
+        else
+        {
+            Console.WriteLine("\nSuma trzech podanych przez Ciebie liczb wynosi " + suma + ". I nie jest podzielna przez 3");
+        }
+
+        }
+    }
